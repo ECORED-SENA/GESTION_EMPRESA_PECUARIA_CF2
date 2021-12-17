aa5fe4b38e10cf696940d5c68696fe0b71505b9e
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-12-17 17:36:05 -0500

    Ajuste tema 1.1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -25,9 +25,51 @@
 
     p.mb-5 El uso de la información es casi interminable y, para cualquier empresa, será la forma de avanzar si le encuentran la forma ideal, tal como se narra a continuación:
 
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)  
+    TabsC.color-acento-contenido
+      .py-3.py-md-4(titulo="La información es base de conocimiento")
+        .row
+          .col-lg-6.mb-4.mb-md-0
+            h4 La información es base de conocimiento
+            p La información persigue como principal propósito el enriquecimiento del conocimiento que se posee sobre algún  aspecto, esto conlleva a reducir los niveles de incertidumbre asociados a la temática en específico, bien sea para prever eventos, de otra forma inesperados, o para comprender todos los sucesos asociados. Extrapolando lo anterior al aspecto administrativo, la información puede aportar elementos de juicio para apoyar la toma de decisiones a cualquier nivel de la organización.        
+          .col-lg-6
+            figure
+              img(src='@/assets/curso/tema1/tab_01.png', alt='',style="width:450px;").m-auto 
+
+      .py-3.py-md-4(titulo="La información es base de conocimiento")
+        .row
+          .col-lg-6.mb-4.mb-md-0
+            h4 Análisis de la información
+            p La información recolectada debe ser sometida a estudio o análisis, de lo contrario, solo será un compendio de datos agrupados sin un propósito establecido. Su interpretación permite la transformación en acciones o toma de medidas, o incluso, en la generación de nuevas ideas. En este aspecto, cabe mencionar que, a mayor calidad de la información recolectada, la precisión de las estrategias basadas en ella, su efectividad y la precisión de la nueva información generada, también será mayor.       
+          .col-lg-6
+            figure
+              img(src='@/assets/curso/tema1/tab_02.png', alt='',style="width:450px;").m-auto
+
+      .py-3.py-lg-4(titulo="Curaduría de la información")
+        .row
+          .col-lg-6.mb-4.mb-md-0
+            h4 Curaduría de la información
+            p La apropiación de los datos dispuestos requiere, a su vez, de un mínimo criterio en la materia objeto de estudio, cualquier persona que efectúe los análisis debe contar con una mentalidad abierta, no solo al aprendizaje, sino al cuestionamiento; en muchas ocasiones, la veracidad de la información debe ser puesta a prueba, dado que existen fuentes de información poco fiables, e incluso, es probable que existan sesgos en los datos si las estrategias utilizadas para su recolección no eran las más recomendables.        
+          .col-lg-6
+            figure
+              img(src='@/assets/curso/tema1/tab_03.png', alt='',style="width:450px;").m-auto
+
+      .py-3.py-md-4(titulo="Valide los datos y las fuentes de información")
+        .row
+          .col-lg-6.mb-4.mb-md-0
+            h4 Valide los datos y las fuentes de información
+            p La toma de decisiones fundamentada en información imprecisa puede desembocar en la aplicación de medidas que no abordan las problemáticas desde la perspectiva correcta, y que por ello, no satisfagan las expectativas; y es que, en la actualidad, existen tantas fuentes de información de fácil acceso, como el Internet, las publicaciones, los estudios académicos, las noticias, entre otras, que se hace obligatorio el contraste entre ellas para determinar si hay coherencia.       
+          .col-lg-6
+            figure
+              img(src='@/assets/curso/tema1/tab_04.png', alt='',style="width:450px;").m-auto 
+
+      .py-3.py-md-4(titulo="Mercados y tendencias")
+        .row
+          .col-lg-6.mb-4.mb-md-0
+            h4 Mercados y tendencias
+            p La oportunidad es otra cualidad imprescindible en la información, puesto que conocer los aspectos relevantes de un escenario específico de forma oportuna permite la toma de decisiones e implementación de medidas a tiempo. Ejemplo de esto es la entrada al mercado de un nuevo producto de la competencia, conocer de ello a tiempo permite implementar estrategias, como campañas publicitarias, para robustecer las ventas de los bienes ofrecidos por la empresa, o incluso la reducción de los precios de venta si con ello se ajustan a las nuevas condiciones de la oferta.        
+          .col-lg-6
+            figure
+              img(src='@/assets/curso/tema1/tab_05.png', alt='',style="width:450px;").m-auto    
 
     separador
 
